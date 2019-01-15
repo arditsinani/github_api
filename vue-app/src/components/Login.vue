d44e69b89c3dd16ad3f59d000d66535c192f6596
rayonit <ardit.sinani@rayonit.com>
2019-01-15 10:15:58 +0100

    fix

--- vue-app/src/components/Login.vue
+++ vue-app/src/components/Login.vue
@@ -2,8 +2,8 @@
   <div class="login-wrapper border border-light">
     <form class="form-signin" @submit.prevent="login">
       <h2 class="form-signin-heading">Sign in to Github</h2>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input v-model="email" type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputEmail" class="sr-only">Username</label>
+      <input v-model="email" type="text" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
       <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
