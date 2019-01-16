718805fd20292c5f5c98f3b20efc600531e8aa28
rayonit <ardit.sinani@rayonit.com>
2019-01-16 10:16:04 +0100

    commit

--- hapijs-final/controllers/github.controller.js
+++ hapijs-final/controllers/github.controller.js
@@ -34,76 +34,89 @@ module.exports = {
                         let starred_urlFull;
                         let starredUrl;
                         profileContent = JSON.parse(body);
-                        if(profileContent){
-                            if(profileContent.starred_url){
-                                starred_urlFull = profileContent.starred_url;
-                                starredUrl = starred_urlFull.substring(0, starred_urlFull.indexOf('{'));
-                            }
+                        let loginName = profileContent.login;
+                        let query1 = {
+                            'profile.login':profileContent.login
                         }
-                        request.get(`${starredUrl}`,{
-                            headers: { 'User-Agent': 'Mozilla/5.0' },
-                            auth: { 
-                                'username':username,
-                                'password':password
-                            }
-                        },(error, response, body) => {
-                            if(error){
-                                reply(error);
-                            }else{
-                                starredContent = JSON.parse(body);
-                                let user = {
-                                        profile:{
-                                            login:profileContent.login,
-                                            id:profileContent.id,
-                                            node_id:profileContent.node_id,
-                                            avatar_url: profileContent.avatar_url,
-                                            gravatar_id: profileContent.gravatar_id,
-                                            url:profileContent.url,
-                                            html_url:profileContent.html_url,
-                                            followers_url: profileContent.followers_url,
-                                            following_url:profileContent.following_url,
-                                            gists_url: profileContent.gists_url,
-                                            starred_url:profileContent.starred_url,
-                                            subscriptions_url: profileContent.subscriptions_url,
-                                            organizations_url: profileContent.organizations_url,
-                                            repos_url: profileContent.repos_url,
-                                            events_url: profileContent.events_url,
-                                            received_events_url: profileContent.received_events_url,
-                                            type: profileContent.type,
-                                            site_admin:profileContent.site_admin,
-                                            name: profileContent.name,
-                                            company: profileContent.company,
-                                            blog: profileContent.blog,
-                                            location: profileContent.location,
-                                            email: profileContent.email,
-                                            hireable: profileContent.hireable,
-                                            bio: profileContent.bio,
-                                            public_repos: profileContent.public_repos,
-                                            public_gists: profileContent.public_gists,
-                                            followers: profileContent.followers,
-                                            following: profileContent.following,
-                                            created_at: new Date(profileContent.created_at),
-                                            updated_at: new Date(profileContent.updated_at),
-                                            private_gists: profileContent.private_gists,
-                                            total_private_repos: profileContent.total_private_repos,
-                                            owned_private_repos: profileContent.owned_private_repos,
-                                            disk_usage: profileContent.disk_usage,
-                                            collaborators: profileContent.collaborators,
-                                            two_factor_authentication: profileContent.two_factor_authentication,
-                                            plan: {
-                                                    name: profileContent.plan.name,
-                                                    space: profileContent.plan.space,
-                                                    collaborators: profileContent.plan.collaborators,
-                                                    private_repos: profileContent.plan.private_repos
-                                            }
-                                        },
-                                        starred:starredContent
+                        GithubModel.find(query1,(err,user1) =>{
+                            let result1 = JSON.stringify(user1);
+                            if(result1 === '[]'){
+                                if(profileContent){
+                                    if(profileContent.starred_url){
+                                        starred_urlFull = profileContent.starred_url;
+                                        starredUrl = starred_urlFull.substring(0, starred_urlFull.indexOf('{'));
+                                    }
                                 }
-                                GithubModel.create(user)    
-                                const token = jwt.sign({ login: username }, config.jwt.secret);
+                                request.get(`${starredUrl}`,{
+                                    headers: { 'User-Agent': 'Mozilla/5.0' },
+                                    auth: { 
+                                        'username':loginName,
+                                        'password':password
+                                    }
+                                },(error, response, body) => {
+                                    if(error){
+                                        reply(error);
+                                    }else{
+                                        starredContent = JSON.parse(body);
+                                        let user = {
+                                                profile:{
+                                                    login:profileContent.login,
+                                                    id:profileContent.id,
+                                                    node_id:profileContent.node_id,
+                                                    avatar_url: profileContent.avatar_url,
+                                                    gravatar_id: profileContent.gravatar_id,
+                                                    url:profileContent.url,
+                                                    html_url:profileContent.html_url,
+                                                    followers_url: profileContent.followers_url,
+                                                    following_url:profileContent.following_url,
+                                                    gists_url: profileContent.gists_url,
+                                                    starred_url:profileContent.starred_url,
+                                                    subscriptions_url: profileContent.subscriptions_url,
+                                                    organizations_url: profileContent.organizations_url,
+                                                    repos_url: profileContent.repos_url,
+                                                    events_url: profileContent.events_url,
+                                                    received_events_url: profileContent.received_events_url,
+                                                    type: profileContent.type,
+                                                    site_admin:profileContent.site_admin,
+                                                    name: profileContent.name,
+                                                    company: profileContent.company,
+                                                    blog: profileContent.blog,
+                                                    location: profileContent.location,
+                                                    email: profileContent.email,
+                                                    hireable: profileContent.hireable,
+                                                    bio: profileContent.bio,
+                                                    public_repos: profileContent.public_repos,
+                                                    public_gists: profileContent.public_gists,
+                                                    followers: profileContent.followers,
+                                                    following: profileContent.following,
+                                                    created_at: new Date(profileContent.created_at),
+                                                    updated_at: new Date(profileContent.updated_at),
+                                                    private_gists: profileContent.private_gists,
+                                                    total_private_repos: profileContent.total_private_repos,
+                                                    owned_private_repos: profileContent.owned_private_repos,
+                                                    disk_usage: profileContent.disk_usage,
+                                                    collaborators: profileContent.collaborators,
+                                                    two_factor_authentication: profileContent.two_factor_authentication,
+                                                    plan: {
+                                                            name: profileContent.plan.name,
+                                                            space: profileContent.plan.space,
+                                                            collaborators: profileContent.plan.collaborators,
+                                                            private_repos: profileContent.plan.private_repos
+                                                    }
+                                                },
+                                                starred:starredContent
+                                        }
+                                        GithubModel.create(user)    
+                                        const token = jwt.sign({ login: loginName }, config.jwt.secret);
+                                        return reply({ token })
+                                    }
+                                });
+                            }else{
+                                const token = jwt.sign({ login: loginName }, config.jwt.secret);
                                 return reply({ token })
                             }
-                        });
+                        })
+                        
                     }
                 })
                     
@@ -118,7 +131,8 @@ module.exports = {
                     if(body.includes('Bad credentials')){
                         reply("Bad credentials").code(404);
                     }else{
-                        const token = jwt.sign({ login: username }, config.jwt.secret);
+                        let profileCont = JSON.parse(body);
+                        const token = jwt.sign({ login: profileCont.login }, config.jwt.secret);
                         return reply({ token });
                     }
                 })
